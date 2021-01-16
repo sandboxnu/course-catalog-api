e3257d87450bece1c7fff5dda0c32f926c2bdf88
Mitch Gamburg <mitch.gamburg@gmail.com>
2021-01-16 16:50:38 -0500

    migrate provider to terraform block

--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -1,7 +1,18 @@
-
 terraform {
   required_version = ">= 0.14"
   required_providers {
     random = ">= 2.2.0"
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "github" {
+  organization = "sandboxnu"
 }
