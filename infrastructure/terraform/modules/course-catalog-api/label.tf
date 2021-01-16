2575435a987190e3fdf947978b16dd0f29eab9dc
Mitch Gamburg <mitch.gamburg@gmail.com>
2021-01-16 16:19:27 -0500

    update cloudposse versions for other services

--- infrastructure/terraform/modules/course-catalog-api/label.tf
+++ infrastructure/terraform/modules/course-catalog-api/label.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.22.0"
   stage      = var.stage
   name       = "course-catalog-api"
   delimiter  = "-"
