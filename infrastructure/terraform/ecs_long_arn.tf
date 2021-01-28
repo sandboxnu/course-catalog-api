c0797a4d4f3f820c63121bbbf5a5c8500c1931f2
Mitch Gamburg <mitch.gamburg@gmail.com>
2021-01-28 09:51:57 -0500

    update terraform again

--- infrastructure/terraform/ecs_long_arn.tf
+++ infrastructure/terraform/ecs_long_arn.tf
@@ -4,9 +4,9 @@ resource "null_resource" "enable_long_ecs_resource_ids" {
   provisioner "local-exec" {
     command = <<EOF
       # enable all ecs long IDs
-      aws ecs put-account-setting-default --name serviceLongArnFormat --value enabled
-      aws ecs put-account-setting-default --name taskLongArnFormat --value enabled
-      aws ecs put-account-setting-default --name containerInstanceLongArnFormat --value enabled
+      aws ecs put-account-setting-default --name serviceLongArnFormat --value enabled --region us-east-1
+      aws ecs put-account-setting-default --name taskLongArnFormat --value enabled --region us-east-1
+      aws ecs put-account-setting-default --name containerInstanceLongArnFormat --value enabled --region us-east-1
 EOF
   }
 }
