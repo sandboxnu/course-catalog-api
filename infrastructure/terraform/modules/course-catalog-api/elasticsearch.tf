c23917c433064e073329e964bf5537639eb46ec8
Mitch Gamburg <mitch.gamburg@gmail.com>
2021-02-01 21:03:52 -0500

    elasticsearch create service-linked role conditionally

--- infrastructure/terraform/modules/course-catalog-api/elasticsearch.tf
+++ infrastructure/terraform/modules/course-catalog-api/elasticsearch.tf
@@ -1,10 +1,10 @@
 # ==================== Elasticsearch cluster ====================
 module "elasticsearch" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=tags/0.27.0"
-  name                    = module.label.name
-  stage                   = module.label.stage
-  security_groups         = [aws_security_group.ecs_tasks.id, var.jumphost_sg_id] # sg that can connect, not sg of ES itself!
-  vpc_id                  = var.vpc_id
+  source          = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=tags/0.27.0"
+  name            = module.label.name
+  stage           = module.label.stage
+  security_groups = [aws_security_group.ecs_tasks.id, var.jumphost_sg_id] # sg that can connect, not sg of ES itself!
+  vpc_id          = var.vpc_id
 
   # Just 1 subnet/availability zone cause ES is dumb $$$
   subnet_ids              = [var.private_subnet_ids[0]]
@@ -18,10 +18,10 @@ module "elasticsearch" {
   encrypt_at_rest_enabled = "false"
 
   // needs to be `false` after generating the role the first time--i.e. after you create staging ES you need to turn this off for prod ES
-  create_iam_service_linked_role = "false"
-  iam_role_arns           = ["*"] // open access is ok because we're in VPC + security group
-  iam_actions             = ["es:*"]
-  kibana_subdomain_name = "kibana-es"
+  create_iam_service_linked_role = module.label.stage == "prod" ? "true" : "false"
+  iam_role_arns                  = ["*"] // open access is ok because we're in VPC + security group
+  iam_actions                    = ["es:*"]
+  kibana_subdomain_name          = "kibana-es"
 
 
   advanced_options = {
