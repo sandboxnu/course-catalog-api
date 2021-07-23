29cfd7d035ebc89d929b066c3bdb626200c3c320
Megan Li <meganlidou@gmail.com>
2021-07-22 22:03:59 -0400

    undo change for debugging aws prod elasticsearch since previous run worked

--- infrastructure/terraform/modules/course-catalog-api/elasticsearch.tf
+++ infrastructure/terraform/modules/course-catalog-api/elasticsearch.tf
@@ -18,7 +18,7 @@ module "elasticsearch" {
   encrypt_at_rest_enabled = "false"
 
   // needs to be `false` after generating the role the first time--i.e. after you create staging ES you need to turn this off for prod ES
-  create_iam_service_linked_role = "false"
+  create_iam_service_linked_role = module.label.stage == "prod" ? "true" : "false"
   iam_role_arns                  = ["*"] // open access is ok because we're in VPC + security group
   iam_actions                    = ["es:*"]
   kibana_subdomain_name          = "kibana-es"
