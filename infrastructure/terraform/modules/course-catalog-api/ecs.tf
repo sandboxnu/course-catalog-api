9c00471aac812ae985c11c3f1aa3547ebcd5132d
Megan Li <meganlidou@gmail.com>
2021-10-17 11:04:46 -0400

    create updater task in staging

--- infrastructure/terraform/modules/course-catalog-api/ecs.tf
+++ infrastructure/terraform/modules/course-catalog-api/ecs.tf
@@ -167,7 +167,7 @@ resource "aws_ecs_service" "update" {
   name            = "${module.label.id}-update"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.update.arn
-  desired_count   = var.stage == "staging" ? 0 : 1
+  desired_count   =  1
   launch_type     = "FARGATE"
 
   network_configuration {
