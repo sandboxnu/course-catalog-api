6f0347fb7c7725451f350177abad228c3c883cc1
hankewyczz <hankewyczz@gmail.com>
2021-10-31 16:40:07 -0400

    Fix sections lastUpdateTime (#47)
    * course serializers
    
    Co-authored-by: hankewyczz <hankewyczz@users.noreply.github.com>
    Co-authored-by: Megan Li <meganlidou@gmail.com>

--- serializers/courseSerializer.js
+++ serializers/courseSerializer.js
@@ -65,6 +65,9 @@ class CourseSerializer {
     // TODO unclear what type Prisma will return for lastUpdateTime
     course.lastUpdateTime = course.lastUpdateTime.getTime();
     course.desc = course.description;
+    course.sections = course.sections.map((section) =>
+      this.serializeSection(section)
+    );
     return this.finishCourseObj(course);
   }
 
