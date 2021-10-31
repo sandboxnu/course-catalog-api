64f125df8290ba0329c0c31a678039f2639b2b50
Megan Li <meganlidou@gmail.com>
2021-10-31 17:10:17 -0400

    fix broken serializers

--- serializers/courseSerializer.js
+++ serializers/courseSerializer.js
@@ -65,9 +65,12 @@ class CourseSerializer {
     // TODO unclear what type Prisma will return for lastUpdateTime
     course.lastUpdateTime = course.lastUpdateTime.getTime();
     course.desc = course.description;
-    course.sections = course.sections.map((section) =>
-      this.serializeSection(section)
-    );
+    if (course.sections) {
+      course.sections = course.sections.map((section) =>
+        this.serializeSection(section)
+      );
+    }
+
     return this.finishCourseObj(course);
   }
 
