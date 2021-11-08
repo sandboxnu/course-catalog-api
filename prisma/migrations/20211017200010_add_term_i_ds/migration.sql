d9376d1f83ea32be8eaa5ea41135e6184c1c8e8c
hankewyczz <hankewyczz@gmail.com>
2021-11-08 15:48:44 -0500

    Merge prisma changes

--- prisma/migrations/20211017200010_add_term_i_ds/migration.sql
+++ prisma/migrations/20211017200010_add_term_i_ds/migration.sql
@@ -1,7 +1,10 @@
 -- CreateTable
 CREATE TABLE "term_ids" (
-    "term_id" TEXT NOT NULL
+    "term_id" TEXT NOT NULL,
+    "sub_college" TEXT NOT NULL,
+    "text" TEXT NOT NULL
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "term_ids.term_id_unique" ON "term_ids"("term_id");
+
