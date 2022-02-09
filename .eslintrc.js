b4dfeaa69201747b6bca341f346f778eadfe5694
hankewyczz <hankewyczz@gmail.com>
2022-02-08 22:51:15 -0500

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,46 @@
 module.exports = {
   env: {
-    es6: true,
+    es2021: true,
     jest: true,
     node: true,
   },
+  // Unless you have a good reason, keep `extends` in the given order
   extends: [
     "eslint:recommended",
+    // Overrides the settings from above ^ which don't apply to Typescript
+    // Keep these 3 in the same order
+    // "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
     "plugin:promise/recommended",
-    "airbnb-base",
-    "airbnb-typescript/base",
+    // "airbnb-base",
+    // "airbnb-typescript/base",
+    // Overrides AirBnB styles; keep in this order
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.eslint.json",
     ecmaVersion: 2021,
   },
-  plugins: ["@typescript-eslint/eslint-plugin", "deprecation"],
+  plugins: ["@typescript-eslint/eslint-plugin", "deprecation", "prettier"],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/camelcase": "off",
-    "deprecation/deprecation": "warn",
-    "eslint/sort-imports": "warn",
-    "esline/no-duplicate-imports": "warn",
+    // // "@typescript-eslint/explicit-function-return-type": "off",
+    // // "@typescript-eslint/explicit-module-boundary-types": "off",
+    // // "@typescript-eslint/camelcase": "off",
+    // "deprecation/deprecation": "warn",
+    // // This is overridden from AirBnB's guide, since they (subjectively) ban for..of loops.
+    // // https://github.com/airbnb/javascript/issues/1122#issuecomment-267580623
+    // "no-restricted-syntax": [
+    //   "error",
+    //   {
+    //     selector: "ForInStatement",
+    //     message:
+    //       "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+    //   },
+    // ],
+    // "no-underscore-dangle": "off",
+    // "no-continue": "off",
+    // "prefer-destructuring": "off",
   },
   overrides: [
     {
