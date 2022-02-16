92f7c6c1cd4cf25f2c9fe4f89c897317b0100782
hankewyczz <hankewyczz@gmail.com>
2022-02-15 23:38:29 -0500

    tests

--- tests/workflow_tests/setup.test.git.ts
+++ tests/workflow_tests/setup.test.git.ts
@@ -9,8 +9,11 @@ describe("Ensure termIDs have been populated", async () => {
   test.todo("termIDs are accessible through GraphQL");
 
   test.todo("The termIDs in the database match those in GraphQL");
-
   //   # psql -U postgres -d searchneu_dev -c 'SELECT * FROM term_ids'
 
   //   # - run: 'curl ''http://localhost:4000/'' -X POST -H ''content-type: application/json'' --data ''{"query": "{ termInfos(subCollege: \"NEU\") { text termId subCollege } }"}'''
 });
+
+// Check that there are courses in the cache
+// Check that the number of courses is consistent - Banner, maybe?
+// Check
