52f9aa076bd495203cc35674e57d62f464e3e08e
hankewyczz <hankewyczz@gmail.com>
2022-02-21 17:58:08 -0500

    DB url interpolation

--- services/prisma.ts
+++ services/prisma.ts
@@ -4,13 +4,7 @@ import macros from "../utils/macros";
 macros.log("** Creating Prisma client");
 let prisma: PrismaClient;
 try {
-  prisma = new PrismaClient({
-    // Prisma doesn't allow string interpolation - https://github.com/prisma/prisma/issues/2559
-    // We set additional variables here
-    datasources: {
-      db: { url: `${process.env.DATABASE_URL}?connection_limit=15` },
-    },
-  });
+  prisma = new PrismaClient();
 } catch (e) {
   macros.error(e);
 }
