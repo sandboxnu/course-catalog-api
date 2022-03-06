d0f3d4988cd8b99982781a10fca7136d0befd441
hankewyczz <hankewyczz@gmail.com>
2022-03-06 17:03:30 -0500

    Fix

--- services/prisma.ts
+++ services/prisma.ts
@@ -2,21 +2,24 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import macros from "../utils/macros";
 
 macros.log("** Creating Prisma client");
-let prisma: any;
+let prisma: PrismaClient;
 try {
-  let prisma = new PrismaClient({
+  prisma = new PrismaClient({
     log: ["info", { level: "error", emit: "event" }, "warn", "error"],
   });
-
   // TEMP / TODO - REMOVE / DO NOT LEAVE HERE PLEASE
   // Temp fix to address Prisma connection pool issues
   // https://github.com/prisma/prisma/issues/7249#issuecomment-1059719644
+  // @ts-expect-error - since we define the type before providing the options, the PrismaClient type is incompelete here
   prisma.$on("error", (e: Prisma.LogEvent) => {
     macros.error(e);
     macros.log("Prisma - handling error");
     macros.log(e.message);
     if (e.message.includes("Timed out fetching a new connection")) {
-      prisma.$disconnect;
+      prisma
+        .$disconnect()
+        .then(() => macros.log("Disconnected from Prisma pool"))
+        .catch((e) => macros.error(e));
     }
   });
 } catch (e) {
