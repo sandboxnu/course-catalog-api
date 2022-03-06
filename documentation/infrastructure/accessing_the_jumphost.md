803208dc0920360452a7712301b0524c67772e6e
hankewyczz <hankewyczz@gmail.com>
2022-03-06 17:37:34 -0500

    Merge master

--- documentation/infrastructure/accessing_the_jumphost.md
+++ documentation/infrastructure/accessing_the_jumphost.md
@@ -1,7 +1,7 @@
 # How to access the Jumphost
 
 1. Generate an SSH key pair on your machine.
-2. Give someone with prod access your public SSH key and have them follow [these instructions](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html) to add new SSH key to the Jumphost.
+2. Give someone with prod access your public SSH key and have them follow [these instructions](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html) to add new SSH key to the Jumphost - see the section on `Add or replace a key pair for your instance`. Basically, add the new public SSH key to `.ssh/authorized_keys` on the jumphost.
 3. On your machine, run `ssh -i <PATH TO SSH PRIVATE KEY> <USER>@<JUMPHOST IP ADDRESS>`. Someone on the team can probably tell you what `USER` and the Jumphost IP address are.
 
 ## Optional
--- services/prisma.ts
+++ services/prisma.ts
@@ -1,14 +1,29 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import macros from "../utils/macros";
 
 let prisma: PrismaClient;
 try {
   prisma = new PrismaClient({
-    log: ["info", "warn", "error"],
+    log: ["info", { level: "error", emit: "event" }, "warn", "error"],
   });
   macros.log("** Created new Prisma client");
+  // TEMP / TODO - REMOVE / DO NOT LEAVE HERE PLEASE
+  // Temp fix to address Prisma connection pool issues
+  // https://github.com/prisma/prisma/issues/7249#issuecomment-1059719644
+  // @ts-expect-error - since we define the type before providing the options, the PrismaClient type is incompelete here
+  prisma.$on("error", (e: Prisma.LogEvent) => {
+    macros.error(e);
+    macros.log("Prisma - handling error");
+    macros.log(e.message);
+    if (e.message.includes("Timed out fetching a new connection")) {
+      prisma
+        .$disconnect()
+        .then(() => macros.log("Disconnected from Prisma pool"))
+        .catch((e) => macros.error(e));
+    }
+  });
 } catch (e) {
   macros.error(e);
 }
 
-export default prisma;
+export default prisma as PrismaClient;
