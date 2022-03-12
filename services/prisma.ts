c74a715ed751a1f55e31e97138c18fb0fc1311d8
hankewyczz <hankewyczz@gmail.com>
2022-03-12 18:13:16 -0500

    Test updates

--- services/prisma.ts
+++ services/prisma.ts
@@ -11,18 +11,21 @@ try {
   // Temp fix to address Prisma connection pool issues
   // https://github.com/prisma/prisma/issues/7249#issuecomment-1059719644
   const intervalTime = 6 * 60 * 60_000; // Every 6 hours
-  setInterval(async () => {
-    const startTime = Date.now();
-    await prisma.$disconnect();
-    macros.log("Disconnected Prisma");
-    await prisma.$connect();
-    const totalTime = Date.now() - startTime;
-    macros.log(
-      `Reconnected Prisma - downtime of ${totalTime} ms (${
-        totalTime / 60_000
-      } mins)`
-    );
-  }, intervalTime);
+
+  if (!macros.TEST) {
+    setInterval(async () => {
+      const startTime = Date.now();
+      await prisma.$disconnect();
+      macros.log("Disconnected Prisma");
+      await prisma.$connect();
+      const totalTime = Date.now() - startTime;
+      macros.log(
+        `Reconnected Prisma - downtime of ${totalTime} ms (${
+          totalTime / 60_000
+        } mins)`
+      );
+    }, intervalTime);
+  }
 } catch (e) {
   macros.error(e);
 }
