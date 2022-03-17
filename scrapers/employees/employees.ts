46b77e36b5d84d1ab219be164101263f98df55a9
hankewyczz <hankewyczz@gmail.com>
2022-03-16 20:12:21 -0400

    Optimizing scraping

--- scrapers/employees/employees.ts
+++ scrapers/employees/employees.ts
@@ -15,39 +15,11 @@ import { v4 as uuidv4 } from "uuid";
 
 const request = new Request("Employees");
 
-const EMPLOYEE_QUERY = {
-  // These values are taken from the API exposed here: https://nu.outsystemsenterprise.com/FSD/
-  // The purpose of these values is unknown, as the API is undocumented. If requests start failing, please
-  // inspect the API response at the above link & update accordingly
-  versionInfo: {
-    moduleVersion: "mWWAP+YL9U4e9B2D7spGFQ",
-    apiVersion: "ad_141F5PYcK3c+D5K8O+g",
-  },
-  viewName: "MainFlow.FacultyAndStaffDirectory",
-  // Their frontend requires non-null queries, but the backend doesn't :)
-  inputParameters: {
-    Input_Name1: "",
-    Input_Name2: "",
-  },
-};
-
 class NeuEmployee {
   people: EmployeeWithId[];
 
   constructor() {
     this.people = [];
-  }
-
-  // Scrapes the page to get the X-CSRF token. This may change - to update, simply visit the page and inspect the request headers
-  // using your browser developer tools
-  async getCsrfToken(): Promise<string> {
-    macros.verbose("NEU Employees - getting X-CSRF token");
-
-    return await request
-      .get({
-        url: "https://nu.outsystemsenterprise.com/FSD/scripts/OutSystems.js",
-      })
-      .then((resp) => resp.body.match(/"X-CSRFToken".*?="(.*?)"/i)[1]);
   }
 
   generateEmployeeId(email?: string): string {
@@ -58,29 +30,72 @@ class NeuEmployee {
   }
 
   parseApiResponse(response: EmployeeRequestResponse[]): EmployeeWithId[] {
-    return response.map((employee) => {
-      return {
-        id: this.generateEmployeeId(employee.Email),
-        name: `${employee.FirstName} ${employee.LastName}`,
-        firstName: employee.FirstName,
-        lastName: employee.LastName,
-        primaryDepartment: employee.Department,
-        primaryRole: employee.PositionTitle,
-        phone: employee.PhoneNumber,
-        emails: [employee.Email],
-        email: employee.Email,
-        officeRoom: employee.CampusAddress,
-        office: employee.CampusAddress,
-      };
-    });
+    return (
+      response
+        .map((employee) => {
+          return {
+            id: this.generateEmployeeId(employee.Email),
+            name: `${employee.FirstName} ${employee.LastName}`,
+            firstName: employee.FirstName,
+            lastName: employee.LastName,
+            primaryDepartment: employee.Department,
+            primaryRole: employee.PositionTitle,
+            phone: employee.PhoneNumber,
+            emails: [employee.Email],
+            email: employee.Email,
+            officeRoom: employee.CampusAddress,
+            office: employee.CampusAddress,
+          };
+        })
+        // Northeastern likes testing in prod (don't we all)
+        // As of 2022-03, they have 4 employees whose names are variations of "do not use"
+        //    Do Not Use, Ed
+        //    Do Not Use Hrm See 000499418, Do Not Use
+        //    DO NOT USE, DO NOT USE
+        .filter(
+          (employee) => !employee.name.toLowerCase().includes("do not use")
+        )
+    );
   }
 
+  /**
+   * Queries the Northeastern employees API.
+   */
   async queryEmployeesApi(): Promise<void> {
-    const csrfToken = await this.getCsrfToken();
+    // These values are taken from the API exposed here: https://nu.outsystemsenterprise.com/FSD/
+    // The purpose of these values is unknown, as the API is undocumented. If requests start failing, please
+    // inspect the API response at the above link & update accordingly
+
+    // Scrapes the source code to get the X-CSRF token.
+    const csrfToken = await request
+      .get({
+        url: "https://nu.outsystemsenterprise.com/FSD/scripts/OutSystems.js",
+      })
+      .then((resp) => resp.body.match(/"X-CSRFToken".*?="(.*?)"/i)[1]);
+
+    const moduleVersion = await request
+      .get({
+        url: "https://nu.outsystemsenterprise.com/FSD/moduleservices/moduleversioninfo",
+      })
+      .then((resp) => resp.body["versionToken"]);
+
+    const employeeQuery = {
+      versionInfo: {
+        moduleVersion,
+        apiVersion: "ad_141F5PYcK3c+D5K8O+g",
+      },
+      viewName: "MainFlow.FacultyAndStaffDirectory",
+      // Their frontend requires non-null queries, but the backend doesn't :)
+      inputParameters: {
+        Input_Name1: "",
+        Input_Name2: "",
+      },
+    };
+
     const response: EmployeeRequestResponse[] = await request
       .post({
         url: "https://nu.outsystemsenterprise.com/FSD/screenservices/FSD/MainFlow/Name/ActionGetContactsByName_NonSpecificType",
-        body: EMPLOYEE_QUERY,
+        body: employeeQuery,
         json: true,
         headers: {
           "X-CSRFToken": csrfToken,
