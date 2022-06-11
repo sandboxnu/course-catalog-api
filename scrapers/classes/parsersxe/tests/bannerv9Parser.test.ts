10adc1ee141a8b42a4f10014891ee34477c5e769
hankewyczz <hankewyczz@gmail.com>
2022-06-10 22:47:16 -0400

    More bannerV9 testing

--- scrapers/classes/parsersxe/tests/bannerv9Parser.test.ts
+++ scrapers/classes/parsersxe/tests/bannerv9Parser.test.ts
@@ -1,8 +1,12 @@
-import request from "../../../request";
 import {
   instance as bannerv9,
   NUMBER_OF_TERMS_TO_UPDATE,
 } from "../bannerv9Parser";
+import filters from "../../../filters";
+import prisma from "../../../../services/prisma";
+import TermParser from "../termParser";
+import classParser from "../classParser";
+import sectionParser from "../sectionParser";
 
 describe("getTermsIds", () => {
   beforeEach(() => {
@@ -61,5 +65,131 @@ describe("getAllTermInfos", () => {
         text: "Summer 2022 Semester",
       },
     ]);
+  });
+});
+
+describe("main", () => {
+  it("customer termIds", () => {
+    const originalScrape = bannerv9.scrapeTerms;
+    bannerv9.scrapeTerms = jest.fn();
+    process.env.TERMS_TO_SCRAPE = "1";
+    bannerv9.main([
+      {
+        subCollege: "CPS",
+        termId: "1",
+        text: "Summer 2022 Semester",
+      },
+    ]);
+    expect(bannerv9.scrapeTerms).toHaveBeenLastCalledWith(["1"]);
+
+    delete process.env.TERMS_TO_SCRAPE;
+    bannerv9.scrapeTerms = originalScrape;
+  });
+
+  it("custom scrape w/ truncate", async () => {
+    process.env.CUSTOM_SCRAPE = "true";
+    filters.truncate = true;
+
+    prisma.course.deleteMany = jest.fn();
+    prisma.section.deleteMany = jest.fn();
+    prisma.$transaction = jest.fn();
+
+    await bannerv9.main([]);
+    expect(prisma.course.deleteMany).toHaveBeenCalled();
+  });
+
+  it("custom scrape without truncate", async () => {
+    process.env.CUSTOM_SCRAPE = "true";
+    filters.truncate = false;
+
+    prisma.course.deleteMany = jest.fn();
+    prisma.section.deleteMany = jest.fn();
+    prisma.$transaction = jest.fn();
+
+    await bannerv9.main([]);
+    expect(prisma.course.deleteMany).not.toHaveBeenCalled();
+  });
+});
+
+it("getCurrentTermInfos", async () => {
+  prisma.course.groupBy = jest.fn().mockReturnValueOnce([
+    {
+      termId: "4",
+    },
+    {
+      termId: "2",
+    },
+    {
+      termId: "1",
+    },
+  ]);
+
+  expect(
+    await bannerv9.getCurrentTermInfos([
+      {
+        subCollege: "NEU",
+        termId: "3",
+        text: "Fall 2022 Semester",
+      },
+      {
+        subCollege: "LAW",
+        termId: "2",
+        text: "Summer 2022 Semester",
+      },
+      {
+        subCollege: "CPS",
+        termId: "1",
+        text: "Summer 2022 Semester",
+      },
+    ])
+  ).toEqual([
+    {
+      subCollege: "LAW",
+      termId: "2",
+      text: "Summer 2022 Semester",
+    },
+    {
+      subCollege: "CPS",
+      termId: "1",
+      text: "Summer 2022 Semester",
+    },
+  ]);
+});
+
+describe("scrapeTerms", () => {
+  it("merges term datas", async () => {
+    // @ts-expect-error -- don't care about the types here
+    TermParser.parseTerm = jest.fn((p, _p) => {
+      const subjects = {};
+      subjects[p] = p;
+      return {
+        classes: [p],
+        sections: [p],
+        subjects,
+      };
+    });
+    expect(await bannerv9.scrapeTerms(["1", "2", "3"])).toEqual({
+      classes: ["1", "2", "3"],
+      sections: ["1", "2", "3"],
+      subjects: { "1": "1", "2": "2", "3": "3" },
+    });
+  });
+});
+
+it("scrapeClass", async () => {
+  // @ts-expect-error -- don't care about the types here
+  classParser.parseClass = jest.fn((termId) => termId);
+  // @ts-expect-error -- don't care about the types here
+  sectionParser.parseSectionsOfClass = jest.fn((termId) => termId);
+
+  expect(await bannerv9.scrapeClass("string", "", "")).toEqual({
+    classes: ["string"],
+    sections: "string",
+  });
+
+  // @ts-expect-error -- don't care about the types here
+  expect(await bannerv9.scrapeClass(false, "", "")).toEqual({
+    classes: [],
+    sections: [],
   });
 });
