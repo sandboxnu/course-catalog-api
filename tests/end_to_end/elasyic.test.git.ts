8dad224b0ceb82518d9136fb16d9a6ef3f74787a
hankewyczz <hankewyczz@gmail.com>
2022-06-14 22:45:19 -0400

    E2E tests

---
+++ tests/end_to_end/elasyic.test.git.ts
@@ -0,0 +1,20 @@
+import classMap from "../../scrapers/classes/classMapping.json";
+import client from "../../utils/elastic";
+
+it("Connections", async () => {
+  expect(await client.isConnected()).toBeTruthy();
+  expect(await client.isConnected()).toBeTruthy();
+  expect(client.closeClient()).toBeFalsy();
+});
+
+it("fetchIndexName", async () => {
+  expect(client["indexes"]["classes"]).toBeUndefined();
+  await client.fetchIndexName("classes");
+
+  expect(client["indexes"]["classes"]).toBe("classes_blue");
+});
+
+it("Creating indexes", async () => {
+  await client.createIndex("indexname", classMap);
+  await client.createIndex("indexname", classMap);
+});
