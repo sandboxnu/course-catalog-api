16a19d7d14dbf8af875d607cba1ff9506ff25c3c
hankewyczz <hankewyczz@gmail.com>
2022-06-25 20:43:46 -0400

    Remove IDs from employee scraping test

--- tests/end_to_end/scraper.test.git.ts
+++ tests/end_to_end/scraper.test.git.ts
@@ -4,8 +4,13 @@ describe("scraping employees", () => {
   it("should be able to query the API and cache it", async () => {
     jest.spyOn(employees, "queryEmployeesApi");
     const result = await employees.main();
+    const result2 = await employees.main();
 
-    expect(await employees.main()).toEqual(result);
+    // The IDs are random for each run, so we remove them.
+    result.forEach((res) => delete res.id);
+    result2.forEach((res) => delete res.id);
+
+    expect(result2).toEqual(result);
 
     // The second time should use the cache
     expect(employees.queryEmployeesApi).toHaveBeenCalledTimes(1);
