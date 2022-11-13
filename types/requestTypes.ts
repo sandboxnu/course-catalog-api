1d393279045e1745d4d340c3e12948e256d9be53
hankewyczz <hankewyczz@gmail.com>
2022-11-13 16:17:29 -0500

    WIP - refactoring up to fireRequest

--- types/requestTypes.ts
+++ types/requestTypes.ts
@@ -1,4 +1,6 @@
 import { CoreOptions } from "request";
+import http from "http";
+import https from "https";
 
 export interface HostAnalytics {
   totalBytesDownloaded: number;
@@ -13,6 +15,8 @@ export interface RequestPool {
   maxSockets: number;
   keepAlive: boolean;
   maxFreeSockets: number;
+  "https:false:ALL"?: http.Agent;
+  "http:"?: http.Agent;
 }
 
 export interface AgentAnalytics {
@@ -28,8 +32,6 @@ export interface CustomRequestConfig extends CoreOptions {
   cacheName?: string;
   headers?: Record<string, string>;
   retryCount?: number;
-  requiredInBody?: string[];
-  shortBodyWarning?: boolean;
   pool?: RequestPool;
 }
 
