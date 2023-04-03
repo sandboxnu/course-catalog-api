e892ab573b0e5b8e82a13c6cd0e316002673499f
hankewyczz <hankewyczz@gmail.com>
2023-04-03 10:50:04 -0400

    Break some configs out of package.json

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+export default {
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  moduleFileExtensions: ["js", "json", "node", "ts"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)(spec|test).[jt]s?(x)"],
+};
