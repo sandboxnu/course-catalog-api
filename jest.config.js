7dc6a6424cf6df2830a5cde04dd2ef47952907b6
hankewyczz <hankewyczz@gmail.com>
2023-04-03 10:54:10 -0400

    Fix jest.config syntax

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   moduleFileExtensions: ["js", "json", "node", "ts"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)(spec|test).[jt]s?(x)"],
