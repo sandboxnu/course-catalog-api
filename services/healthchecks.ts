50023bd18edf807e6b4ac8b02addb660e3f62701
cherman27 <carterherman27@gmail.com>
2024-10-31 11:28:52 -0400

    Modify to use macros when healthchecks does not respond.

--- services/healthchecks.ts
+++ services/healthchecks.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import macros from "../utils/macros";
 
 export async function sendHealthCheck(): Promise<void> {
   const pingURL = "	https://hc-ping.com/7d9197be-db67-4cc5-be00-1a3df56d0918";
@@ -9,6 +10,6 @@ export async function sendHealthCheck(): Promise<void> {
   try {
     await axios.get(pingURL);
   } catch (error) {
-    console.log("Health checks service is not responding properly " + error);
+    macros.warn("Health checks service is not responding properly: " + error);
   }
 }
